f334e5a08b01a4e5b1f9ad00494d9f62112b9dca
Gregory Tziatzios <grigoriot@csd.auth.gr>
2021-04-08 19:30:29 +0300

    Fixed Bugs

--- Assets/Scripts/ControlCanvas.cs
+++ Assets/Scripts/ControlCanvas.cs
@@ -1,5 +1,12 @@
 using UnityEngine;
 
+/*
+ * This script controls how the Canvas for the in game will work. Basically it checks to see if we have paused
+ * the game or if there is anything we need to update in the player's screen.
+ *
+ * Αυτό το script ελέγχει το πως ο canvas για το παιχνίδι θα δουλέυει. Με λίγα λόγια ελέγχει άμα το παιχνίδι έχει
+ * γίνει paused ή άμα υπάρχει κάτι που πρέπει να ανανεώσουμε στην οθόνη του χρήστη.
+ */
 public class ControlCanvas : MonoBehaviour
 {
     public GameObject inGameMenu;
--- Assets/Scripts/InGameMenu.cs
+++ Assets/Scripts/InGameMenu.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/*
+ * This script represents the In-Game Menu and basically holds the functions that are going to be used by the
+ * buttons in the menu.
+ *
+ * Αυτό το script αναπαραστά το In-Game Menu και κρατά τις συναρτήσεις που θα χρησιμοποιηθούν από τα buttons στο
+ * μενού.
+ */
 public class InGameMenu : MonoBehaviour
 {
     public void LoadLevel(int level)
